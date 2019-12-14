74a2f803a24ec959774e2933a7ed37555f7be3eb
Kayla Kantola <krkantola@gmail.com>
2019-12-13 21:58:24 -0500

    it works

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const getHtmlFromPages = require("./lib/get-html-from-pages");
 const getLinksFromHTMLs = require("./lib/get-links-from-htmls");
 const getLinkStatuses = require("./lib/get-link-statuses");
-const { map } = require("ramda");
+const findBadLinks = require("./lib/find-bad-links");
+const { map, unnest } = require("ramda");
 
 const linkChecker = links =>
   getHtmlFromPages(links)
     .then(res => getLinksFromHTMLs(res))
     .then(res => Promise.all(map(r => getLinkStatuses(r), res)))
+    .then(res => unnest(res))
+    .then(res => findBadLinks(res))
     .then(res => console.log(res))
     .catch(e => console.log(e));
 
-const fakelinks = ["https://www.facebook.com", "https://www.google.com"];
-
-linkChecker(fakelinks);
+// const fakelinks = ["https://www.facebook.com", "https://www.google.com"];
+//
+// linkChecker(fakelinks);
