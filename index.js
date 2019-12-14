11ad85e525256f3498e1ebb538b819523bc07596
Kayla Kantola <krkantola@gmail.com>
2019-12-13 21:45:23 -0500

    gets links

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const getHtmlFromPages = require("./lib/get-html-from-pages");
-const getLinksFromHTML = require("./lib/get-links-from-html");
+const getLinksFromHTMLs = require("./lib/get-links-from-htmls");
 const checkLinks = require("./lib/check-links");
 const { map } = require("ramda");
 
 const linkChecker = links =>
   getHtmlFromPages(links)
-    .then(res => console.log({ res }))
+    .then(res => getLinksFromHTMLs(res))
+    .then(res => console.log(res))
     .catch(e => console.log(e));
 
 const fakelinks = ["https://www.facebook.com", "https://www.google.com"];
